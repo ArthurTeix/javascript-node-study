ea99d314b55a31aea9fff2bc0db5995f62cdff03
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-17 15:06:31 -0300

    docs: modules export

--- modulos/1/app.js
+++ modulos/1/app.js
@@ -1,3 +1,9 @@
-const { nome, sobrenome, falaNome } = require('./mod1')
-
+/*const { nome, sobrenome, falaNome } = require('./mod1')
 console.log(nome, sobrenome, falaNome())
+*/
+
+const { nome, sobrenome, idade, Pessoa } = require('./mod1')
+
+const p1 = new Pessoa('Ingrid')
+
+console.log(p1, '-', nome, '-', sobrenome, '-', idade)
--- modulos/1/mod1.js
+++ modulos/1/mod1.js
@@ -1,4 +1,4 @@
-const nome = 'Arthur'
+/*const nome = 'Arthur'
 const sobrenome = 'Teixeira'
 
 function falaNome() {
@@ -7,3 +7,16 @@ function falaNome() {
 
 // exportar apenas oq quero
 module.exports = { nome, sobrenome, falaNome }
+*/
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome    
+    }
+}
+
+const nome = 'Arthur'
+const sobrenome = 'Teixeira'
+
+module.exports = { nome, sobrenome, Pessoa }
+module.exports.idade = 18
