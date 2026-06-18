609905d27da583bfcedc053400f976a6ddc66504
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-18 16:53:15 -0300

    docs: flag "a"

--- fileSystem/modules/write.js
+++ fileSystem/modules/write.js
@@ -5,5 +5,7 @@ const path = require('path')
 const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
 
 // aqui estou enviando o que deve ser escrito no meu arquivo ('frase 1')
-// a flag serve para apagar tudo que está escrito no meu arquivo caso ele já exista
+// a flag 'w' serve para apagar tudo que está escrito no meu arquivo caso ele já exista
 fs.writeFile(caminhoArquivo, 'Frase 1', { flag: 'w' })
+
+// caso eu queira um comportamento de append no arquivo (ir sempre adicionando, basta alterar a flg para 'a')
