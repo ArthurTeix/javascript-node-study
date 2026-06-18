77a6bfce6afa3e33005bc397d3b9212a0fc106a0
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-18 20:51:50 -0300

    feat: create write function

---
+++ fileSystem/app.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const caminhoArquivo = path.resolve(__dirname, 'teste.json')
+const escreve = require('./modules/write')
+
+const pessoas = [
+    {nome: 'Arthur'},
+    {nome: 'Ingrid'},
+    {nome: 'Miguel'},
+    {nome: 'Lucas'}
+]
+
+// converte meu obj em um arquivo de texto json
+const json = JSON.stringify(pessoas, '', 2)
+
+escreve(caminhoArquivo, json)
--- fileSystem/modules/write.js
+++ fileSystem/modules/write.js
@@ -1,11 +1,13 @@
 const fs = require('fs').promises
-const path = require('path')
 
 // aqui estou direcionando onde será criado o meu arquivo de texto chamado teste (uma pasta antes, na raiz)
-const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
+// -> const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
 
 // aqui estou enviando o que deve ser escrito no meu arquivo ('frase 1')
 // a flag 'w' serve para apagar tudo que está escrito no meu arquivo caso ele já exista
-fs.writeFile(caminhoArquivo, 'Frase 1', { flag: 'w' })
-
 // caso eu queira um comportamento de append no arquivo (ir sempre adicionando, basta alterar a flg para 'a')
+// -> fs.writeFile(caminhoArquivo, 'Frase 1', { flag: 'w' })
+
+module.exports = (caminho, dados) => {
+    fs.writeFile(caminho, dados, { flag: 'w' })
+}
