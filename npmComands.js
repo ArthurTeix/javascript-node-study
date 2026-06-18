002ba21838018e6cbac41e12aabf417dc8bb15be
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-18 15:02:43 -0300

    docs: add notes comands npm

---
+++ npmComands.js
@@ -0,0 +1,40 @@
+/*
+Para iniciar o projeto devo instalar o package.json 
+    -> npm i -y || npm install -y
+
+Para implementar o node_modules
+    -> npm i
+
+Para instalar algum pacote ou módulo basta usar o comando de instalação + o nome do pacote
+    -> npm i express
+
+Para instalar um pacote ou módulo que não quero que atualize
+    -> npm i express -E
+
+Para atualizar meus pacotes e dependências
+    -> npm update
+
+Para jogar um pacote para devDependencies (dependências de desenvolvedor)
+    -> npm i express --save-dev
+
+Para jogar um arquivo devolta para as dependencies (dependências de produção)
+    -> npm i express --save-prod
+
+Para instalar uma versão específica do meu pacote uso o @ após seu nome
+    -> npm i express@2.1.0
+
+Para instalar a versão mais recente do meu major (primeiro número da versão, por ex: 2.1.0, 2 = majo | 1 = minor | 0 = patch), com o minor e o patch mais recentes
+    -> npm i express@4.x
+
+Para desinstalar algum arquivo
+    -> npm uninstall express
+
+Para poder ver o que eu instalei 
+    -> npm ls
+
+Para escolher quantas camadas quero ver do que instalei 
+    -> npm ls --depth=nº (nº = número de camadas que deseja ver, se quiser apenas superficial coloca 0)
+
+Para retornar as dependências desatualizadas
+    -> npm outdated
+*/
