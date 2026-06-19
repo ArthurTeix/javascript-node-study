e662f17dfa571cb83aeeaf56810df8e3d42dd54b
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-18 21:06:03 -0300

    docs: add notes write and read files

--- fileSystem/app.js
+++ fileSystem/app.js
@@ -4,13 +4,23 @@ const caminhoArquivo = path.resolve(__dirname, 'teste.json')
 const escreve = require('./modules/write')
 const ler = require('./modules/read')
 
-const pessoas = [
-    {nome: 'Arthur'},
-    {nome: 'Ingrid'},
-    {nome: 'Miguel'},
-    {nome: 'Lucas'}
-]
+// const pessoas = [
+//     {nome: 'Arthur'},
+//     {nome: 'Ingrid'},
+//     {nome: 'Miguel'},
+//     {nome: 'Lucas'}
+// ]
 
 // converte meu obj em um arquivo de texto json
-const json = JSON.stringify(pessoas, '', 2)
-escreve(caminhoArquivo, json)
+
+// APÓS ESCREVER O ARQUIVO, POSSO COMENTAR E APENAS LER AGORA 
+// const json = JSON.stringify(pessoas, '', 2)
+// escreve(caminhoArquivo, json)
+
+async function lerArquivo(caminho) {
+    const dados = await ler(caminho)
+    return dados
+}
+
+const dadosArquivo = lerArquivo(caminhoArquivo)
+    .then(dados => console.log(dados))
