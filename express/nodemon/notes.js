87fb06a619dc7acded8e688e75a5422998ce2304
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-23 16:35:42 -0300

    docs: add notes about nodemon

---
+++ express/nodemon/notes.js
@@ -0,0 +1,11 @@
+/*
+O nodemon serve como um atualizador automático do meu server, para usar é simples.
+
+1- npm init -y (instalar o package.json)
+2- npm i express (instalar express)
+3- npm i nodemon --save-dev (instalar nodemon nas devDependencies)
+4- ir no start do meu package.json (criar abaixo de "test" em "script" se necessário) e alterar de 'node server.js' para 'nodemon server.js'
+5- ir no terminal e digitar 'npm start'
+
+A partir daí, o nodemon ficará assistindo meu arquivo e a cada alteração, irá atualizar
+*/
