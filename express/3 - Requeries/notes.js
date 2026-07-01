57c8ce99b2116b1d422d01521591205927d4bf62
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-01 16:18:53 -0300

    docs: add notes about requeries

---
+++ express/3 - Requeries/notes.js
@@ -0,0 +1,11 @@
+/*
+
+req.params -> são parâmetros que vem na rota do site (na url)
+    ex: http://localhost:3000/profiles/123
+
+req.query -> são parâmetros que vem na query string da sua url, acompanhados de ? e &
+    ex: http://localhost:3000/profiles/?nome=Arthur&sobrenome=Teixeira&idade=18
+
+req.body -> são parâmetros que vem no corpo (body) da requisição
+    ex: dados advindos de um input da página
+*/
