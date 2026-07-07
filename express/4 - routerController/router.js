9f1894b2f951f2f9d2c8c842660fac522a35d879
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-07 17:13:05 -0300

    feat: add route contato

--- express/4 - routerController/router.js
+++ express/4 - routerController/router.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const route = express.Router()
 
-// importando controller da pag inicial
+// importando controller da home
 const homeController = require('./controllers/homeController.js')
+
+// importando controller da pág de contatos
+const contatoController = require('./controllers/contatoController.js')
 
 // organizando melhor o arquivo de rotas, removendo as funções usando elas importadas
 // rotas da home
 route.get('/', homeController.paginaInicial)
 route.post('/', homeController.trataPost)
+
+// rotas de contato
+route.get('/contato', contatoController.paginaInicial)
 
 // exportando a const route para usar no server.js
 module.exports = route  
