448c01da3a9936f7caee851d984c7b4b1c35493d
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-12 16:46:55 -0300

    docs: add notes about files statics

--- express/6 - arquivosEstaticos/notes.js
+++ express/6 - arquivosEstaticos/notes.js
@@ -2,3 +2,7 @@
 // Logos, Imagens, Styles, Scripts e etc 
 
 // Os arquivos estáticos ficam na pasta public 
+
+// sempre que quiser encontrar algum arquivo estático direto na página, adiciono o caminho na url, sem contar a pasta public
+
+// ex: http://localhost:3000/teste.txt -> mesmo o caminho sendo ./public/teste.txt, mencionamos apenas o teste.txt isolado
