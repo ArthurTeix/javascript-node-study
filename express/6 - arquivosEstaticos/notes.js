ebff043993225aaaa4826e04507a1f5aa9a05912
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-12 16:30:03 -0300

    initial notes

---
+++ express/6 - arquivosEstaticos/notes.js
@@ -0,0 +1,2 @@
+// Arquivos estáticos são conteúdos carregados no front-end ao iniciar a página
+// Logos, Imagens, Styles, Scripts e etc 
---
+++ express/6 - arquivosEstaticos/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// importando rotas
+const routes = require('./router')
+
+
+app.use(express.urlencoded({ extended: true }))
+
+// caminho absoluto para a pasta views
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+
+app.set('view engine', 'ejs')
+
+// usando rotas
+app.use(routes)
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+    console.log(`Acessar http://localhost:3000`)
+})
