360a99a7941e8f4fd267747d0a1f4e79ae3b6f45
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-13 23:49:34 -0300

    feat: conect webpack + express

--- express/7 - webpack/webpack.config.js
+++ express/7 - webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './frontend/index.js',
     output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js'
@@ -15,7 +15,9 @@ module.exports = {
         use: {
             loader: 'babel-loader',
             options: {
-                presets: [['@babel/preset-env']],  // ← corrigido
+                presets: [ // para que a modularização funcione, devo add que os módulos devem ser compilados pelo commonjs
+                    ['@babel/preset-env', { modules: 'commonjs' }]
+                ],
                 sourceType: 'module'
             }
         }
