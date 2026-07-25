361cd105a85a20b228422d901c6f0c888cc6a664
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-24 21:13:59 -0300

    feat: create cluster in mongoDB

--- mongoDb/1 - conexao/src/controllers/homeController.js
+++ mongoDb/1 - conexao/src/controllers/homeController.js
@@ -1,3 +1,13 @@
+const HomeModel = require('../models/HomeModel.js')
+
+// isso retorna uma promisse
+HomeModel.create({
+    titulo: "Teste",
+    descricao: 'estou apenas testando!'
+})
+    .then(dados => console.log(dados))
+    .catch(err => console.log(err))
+
 exports.paginaInicial = (req, res) => {
     res.render('index')
     return
