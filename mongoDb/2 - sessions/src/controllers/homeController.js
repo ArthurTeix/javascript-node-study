5511f032a7d2d5d52a8264f94e4bdfbdfd1c9f52
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-25 22:24:55 -0300

    docs: add exemples sessions and flashs

--- mongoDb/2 - sessions/src/controllers/homeController.js
+++ mongoDb/2 - sessions/src/controllers/homeController.js
@@ -1,4 +1,13 @@
 exports.paginaInicial = (req, res) => {
+    //  EXEMPLO DE SESSIONS
+    // req.session.usuario = { nome: 'arthur', idade: 18 } <- esses dados ficarão salvos por 7 dias
+    // console.log(req.session.usuario) e sempre serão exibidos no console após esse comando
+
+    //  EXEMPLO DE FLASH <- mensagem a ser exibida apenas uma vez (não é armazenada)
+    // req.flash('info', "informação")
+    // req.flash('err', "Algo deu errado")
+    // req.flash('sucess', "Sucesso!")
+    // console.log(req.flash('info'), req.flash('err'), req.flash('sucess'))
     res.render('index')
     return
 } 
