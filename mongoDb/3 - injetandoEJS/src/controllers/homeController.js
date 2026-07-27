93d5cc5d0ccd9a5c01135711a4906ae253298fd0
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-27 15:28:13 -0300

    feat: injeted title ejs

--- mongoDb/3 - injetandoEJS/src/controllers/homeController.js
+++ mongoDb/3 - injetandoEJS/src/controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.paginaInicial = (req, res) => {
     res.render('index', { // posso injetar dados na minha página ejs direto na sua renderização
-        titulo: "Aqui fica o título da minha página",
+        titulo: "Aqui fica o <span style='color: red'>título</span> da minha página",
         numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
     })
     return
