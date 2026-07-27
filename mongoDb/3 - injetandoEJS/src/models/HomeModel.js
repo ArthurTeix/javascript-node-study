15f83aa848949363f98858998e89245446d0295b
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-27 15:05:30 -0300

    initial notes ejs

---
+++ mongoDb/3 - injetandoEJS/src/models/HomeModel.js
@@ -0,0 +1,11 @@
+// O nome do arquivo começa com letras maiúsculas pois modelos são classes
+
+const mongoose = require('mongoose')
+
+// vou criar o esquema da minha home, passando atributos e tipos de dados que desejo
+const HomeSchema = new mongoose.Schema({
+    titulo: { type: String, required: true},
+    descricao: String
+})
+
+const HomeModel = mongoose.model("Home", HomeSchema)
