9bb89774e852055b85ad1a4956103af6a0a85119
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-27 15:36:44 -0300

    feat: injeted mesage in ejs

--- mongoDb/3 - injetandoEJS/src/controllers/homeController.js
+++ mongoDb/3 - injetandoEJS/src/controllers/homeController.js
@@ -1,7 +1,7 @@
 exports.paginaInicial = (req, res) => {
     res.render('index', { // posso injetar dados na minha página ejs direto na sua renderização
         titulo: "Aqui fica o <span style='color: red'>título</span> da minha página",
-        numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        mensagem: "Mensagem para exibir de teste"
     })
     return
 } 
