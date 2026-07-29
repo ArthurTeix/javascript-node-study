acc58b7ee71882c48de2f28292dffe38114a27b8
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-28 23:26:40 -0300

    initial notes

--- mongoDb/3 - injetandoEJS/src/controllers/homeController.js
+++ mongoDb/3 - injetandoEJS/src/controllers/homeController.js
@@ -1,7 +1,8 @@
 exports.paginaInicial = (req, res) => {
     res.render('index', { // posso injetar dados na minha página ejs direto na sua renderização
         titulo: "Aqui fica o <span style='color: red'>título</span> da minha página",
-        mensagem: "Mensagem para exibir de teste"
+        mensagem: "Mensagem para exibir de teste",
+        numeros: [1, 2, 3, 4, 5, 6, 7]
     })
     return
 } 
