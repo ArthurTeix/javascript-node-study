840eee718db4f72508a102dcd5350b858f464718
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-01 17:11:30 -0300

    feat: install helmet and csrf

--- mongoDb/4 - helmet/server.js
+++ mongoDb/4 - helmet/server.js
@@ -8,7 +8,6 @@ const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose')
-
 mongoose.connect(process.env.CONNECTIONSTRING)  
     .then(() => {
         app.emit('Pronto para conexão')
@@ -18,7 +17,10 @@ mongoose.connect(process.env.CONNECTIONSTRING)
 const session = require('express-session')
 const { MongoStore } = require('connect-mongo')
 const flash = require('connect-flash')
+const routes = require('./router')
+const path = require('path')
 
+const { middlewareGlobal } = require('./src/middlewares/middleware.js')
 const sessionOptions = session({
     secret: 'texto que ninguém vai ler',
     store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING}),
@@ -30,23 +32,18 @@ const sessionOptions = session({
     }
 })
 
+
 app.use(sessionOptions)
 app.use(flash())
-
-const routes = require('./router')
-const path = require('path')
-const { middlewareGlobal } = require('./src/middlewares/middleware.js')
-
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, 'public')))
 
-app.set('views', path.resolve(__dirname, 'src', 'views'))
-app.set('view engine', 'ejs')
-
-// nossos próprios middlewares
 app.use(middlewareGlobal)
 app.use(routes)
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
 
 app.on('Pronto para conexão', () => {
     app.listen(3000, () => {
